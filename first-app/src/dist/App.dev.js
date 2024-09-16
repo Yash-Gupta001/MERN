568f59e5e0ad70bafe085a13021bdaa86b1eeab7
Yash-Gupta001 <gyash4963@gmail.com>
2024-09-16 14:29:43 +0530

    name update

---
+++ first-app/src/dist/App.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var App = function App() {
+  return;
+};
+//# sourceMappingURL=App.dev.js.map
---
+++ first-app/src/dist/index.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _client = _interopRequireDefault(require("react-dom/client"));
+
+var _App = _interopRequireDefault(require("./App"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var root = _client["default"].createRoot(document.getElementById('root'));
+
+root.render();
+//# sourceMappingURL=index.dev.js.map
